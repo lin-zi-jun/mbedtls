dbf8baa70fea02b5d1b767e3534f3608bc12b9ee
jordan <857413240@qq.com>
2020-12-13 13:57:57 +0800

    套间类型说明

--- Jordan/Ciphersuite-list/list.c
+++ Jordan/Ciphersuite-list/list.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "mbedtls/ssl.h"
 
+
+// 密钥协商算法 ECDHE
+// 身份认证算法 ECDSA
+// 对称加密算法 AES_256
+// 消息认证算法 GCM
+// 伪随机数算法 SHA384
+
+
+
 int main(void){
     int  index = 1;
     const int  *list;
