07175eee325568f20279f3fd7a66c927bd6f9032
jordan <857413240@qq.com>
2020-12-14 11:23:31 +0800

    添加 HMAC-SHA256 说明

--- Jordan/sha_256/test.c
+++ Jordan/sha_256/test.c
@@ -3,6 +3,25 @@
 #include "mbedtls/md.h"
 #include "mbedtls/platform.h"
 
+
+
+// 消息认证码（Massage Authentication Code）用来检查消息的完整性和真实性。
+// 消息认证码的输入为任意长度的消息和发送者与接收者之间共享的密钥，输出为固定长度的数据，该数据被称作MAC值、Tag或T。
+// 发送者与接收者判断这个MAC判断消息完整性和真实性
+
+// 消息认证码的实现
+
+// 单项散列算法实现，这类方法统称为HMAC，
+// 与SHA1算法结合称为HMAC-SHA1，
+// 与SHA256算法结合称为HMAC-SHA256，
+// 与MD5算法结合称为HMAC-MD5
+
+// 分组密码实现，采用CBC模式称作CBC-MAC，还有CMAC
+// 认证加密算法实现，是对称加密算法和消息认证码的结合，有GCM和CCM
+
+// HMAC-SHA256例子
+
+
 int main(void){
     u_int8_t digest[32];
     char *msg = "abc";
